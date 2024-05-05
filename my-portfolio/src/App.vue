5db32e455c5adbb4bd46325dfca4e23d2fbb352a
Agent47 <dkadzutu@gmail.com>
2024-05-05 17:51:08 +0100

    2nd branch test to main/master

--- my-portfolio/src/App.vue
+++ my-portfolio/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App3"/>
+  <HelloWorld msg="Testing how pull requests work"/>
 </template>
 
 <script>
